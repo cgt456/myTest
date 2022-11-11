f3c57a76fec78f682f44cf269b572934a1d3d4bc
cgt <867454091@qq.com>
2022-11-12 00:07:23 +0800

    msg

--- test1.c
+++ test1.c
@@ -3,7 +3,7 @@ int main( void )
 {
 	printf("hello world!!!\n");
 	printf("add something 20221111");
-	printf("add something 20221111");
+///	printf("add something 20221111");
 	
 ///	printf("add something 20221111");
 
